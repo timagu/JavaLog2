ef5cbba47047aefcf82c08a3f5f1fd9200f65634
幸太 <kouta0810277@gmail.com>
2018-02-28 22:48:02 +0900

    変数テスト

--- app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int num = 50;
+        int num = 10;
         Log.d("javatest",String.valueOf(num));
 
         num = 50;
