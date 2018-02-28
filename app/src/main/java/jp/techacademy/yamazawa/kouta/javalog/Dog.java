31d563e7ca6a836e6eef32f338f1d8bcc56c9725
幸太 <kouta0810277@gmail.com>
2018-03-01 00:39:16 +0900

    LESSON3 7.2

---
+++ app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/Dog.java
@@ -0,0 +1,23 @@
+package jp.techacademy.yamazawa.kouta.javalog;
+
+/**
+ * Created by 幸太 on 2018/03/01.
+ */
+import android.util.Log;
+
+class Dog {
+    //変数
+    String name;    //名前
+    int age;    //年齢
+
+    //コンストラクタ
+    public Dog(){
+        name = "";
+        age = 0;
+    }
+
+    //メソッド
+    public void say(){
+        Log.d("javatest", this.name + "(" + this.age + "歳)" + "「ワンワン」");
+    }
+}
