e0aae3b2bcc359203e20dd29b14feaade7269add
幸太 <kouta0810277@gmail.com>
2018-02-28 23:08:31 +0900

    演算テスト

--- app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/MainActivity.java
@@ -11,10 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int num = 10;
-        Log.d("javatest",String.valueOf(num));
+        int num = 10+5-2*4/2;
+        Log.d("javatest","10+5-2*4/2=" + String.valueOf(num));
 
-        num = 50;
-        Log.d("javatest",String.valueOf(num));
+        boolean flag1 = true;
+        boolean flog2 = false;
+        Log.d("javatest","flag1 && flag2 = " + Strijng.valueof(flag1 && flag2));
+        Log.d("javatest","flag1 || flag2 = " + Strijng.valueof(flag1 || flag2));
+
+        int num2 = 10;
+        int num3 = 20;
+        Log.d("javatest","num2 < num3 = " + Strijng.valueof(num2 < num3));
     }
 }
