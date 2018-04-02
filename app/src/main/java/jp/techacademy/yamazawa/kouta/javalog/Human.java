3a7f67c83dc8bf06427e736feb2708b560f95cb3
KoutaYamazawa <kouta0810277@gmail.com>
2018-04-02 17:23:10 +0900

    LESSON3 7.3課題提出
    よろしくお願い致します。

--- app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/Human.java
+++ app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/Human.java
@@ -7,9 +7,15 @@ import android.util.Log;
  */
 
 public class Human extends Animal implements Thinkable{
+    static String name = "幸太";
+    static int age = 34;
     static String hobby = "趣味";
     public void say() {
         Log.d("javatest", "私の名前は"+ name + "です。");
         Log.d("javatest", "年は"+ age + "歳です。");
+    }
+
+    public void think(){
+        Log.d("javatest", "私は" + Human.hobby + "について考える");
     }
 }
