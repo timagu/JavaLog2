9c86cbcd883d492992088beb7c089258b6ce211c
KoutaYamazawa <kouta0810277@gmail.com>
2018-04-04 16:44:48 +0900

    Lesson3課題再提出

--- app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/Human.java
+++ app/src/main/java/jp/techacademy/yamazawa/kouta/javalog/Human.java
@@ -12,20 +12,17 @@ public class Human extends Animal implements Thinkable{
 
     public Human(String name, int age, String hobby){
         //コンストラクタ
-        name="";
-        age = 0;
-        hobby="";
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
     }
-
-    // 名前を幸太、年齢34歳で、Humanのインスタンスを作る
-    Human human/* = new Human("幸太", 34, "趣味")*/;
 
     //名前と年を言う。
     public void say(){
-        Log.d("javatest", "私の名前は" + human.name + "です。年は" + human.age + "歳です。");
+        Log.d("javatest", "私の名前は" + name + "です。年は" + age + "歳です。");
     }
     //趣味を言う。
     public void think(){
-        Log.d("javatest", "私は" + human.hobby + "について考える");
+        Log.d("javatest", "私は" + hobby + "について考える");
     }
 }
